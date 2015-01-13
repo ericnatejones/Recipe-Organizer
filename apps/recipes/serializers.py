4033aef3ced44c595ae0a4d354482b5d269c03eb
Eric Jonese <ericnatejones@gmail.com>
2015-01-13 14:28:21 -0700

    updating repo with a separated front end and back end

---
+++ apps/recipes/serializers.py
@@ -0,0 +1,16 @@
+# __author__ = 'macuser'
+# from rest_framework import serializers
+# from models import *
+#
+#
+# class RecipeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Recipe
+#         depth = 1
+
+l = [x * 2 for x in range(1,10) if x % 3 == 0]
+
+print l
+
+
+
